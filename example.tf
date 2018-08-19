d4442421b17f871d8ccbe7331d9e5fece0c0f5f5
Rick Alvarez <rickalv.it@gmail.com>
2018-08-18 22:43:10 -0400

    Adding s3 bucket with dependencies and an extra instance

--- example.tf
+++ example.tf
@@ -3,7 +3,24 @@ provider "aws" {
   version = "~> 1.32"
 }
 
+# New resource for the S3 bucket our application will use.
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "devops.miami"
+  acl    = "private"
+}
+
 resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+  # Tells Terraform that this EC2 instance must be created only after the
+  # S3 bucket has been created.
+  depends_on = ["aws_s3_bucket.example"]
+}
+
+resource "aws_instance" "another" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 }
