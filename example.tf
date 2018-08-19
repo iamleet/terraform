1e469be04ac077825df9b8d9f27c396f4aea6710
Rick Alvarez <rickalv.it@gmail.com>
2018-08-18 22:57:04 -0400

    Removing the extra instance. Adding a local provisioner that runs on instance creation

--- example.tf
+++ example.tf
@@ -18,11 +18,10 @@ resource "aws_instance" "example" {
   # Tells Terraform that this EC2 instance must be created only after the
   # S3 bucket has been created.
   depends_on = ["aws_s3_bucket.example"]
-}
 
-resource "aws_instance" "another" {
-  ami           = "ami-b374d5a5"
-  instance_type = "t2.micro"
+  provisioner "local-exec" {
+  command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
 
 resource "aws_eip" "ip" {
