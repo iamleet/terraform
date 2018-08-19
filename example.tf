53d4a4bd7f0b1bdd88292b367e89142c724e4b06
Rick Alvarez <rickalv.it@gmail.com>
2018-08-18 21:18:46 -0400

    Use Redhat🐧, lock the version in

--- example.tf
+++ example.tf
@@ -1,8 +1,9 @@
 provider "aws" {
   region     = "us-east-1"
+  version = "~> 1.32"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-6871a115"
   instance_type = "t2.micro"
 }
